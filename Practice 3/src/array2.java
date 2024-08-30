ec9cc9d75d5fc2f7b731952694a321a887f67caf
Prajjwal Rimal <rimalprajjwal1@gmail.com>
2024-08-30 17:58:09 +0545

    JAVA SELF LEARNING
    JAVA SELF LEARNING

---
+++ Practice 3/src/array2.java
@@ -0,0 +1,11 @@
+//2. Explain the need for arrays in Java with an example of managing student grades.
+
+// arrays are needed in java as they can store multiple values of the same type in a memory side by side
+// lets say we need to manage the data of students grade what we can do is we an create
+// two array students[] and marks[] then we can add value to the array through index calling by loops
+// we can also print the array using loops this makes data management easy and all the data will be stored side by
+// side in the memory.
+
+// if we were to not create the arrays we would have to use String name and int marks for each number of students
+// making the code hard to read and making variable naming difficult also each of the variables would have diffrent
+// memory addresses and not a continious one like in array. 
