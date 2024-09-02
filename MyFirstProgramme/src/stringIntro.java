063b3ea69d6f83dcad00dae2fe4411ae19c06705
Prajjwal Rimal <rimalprajjwal1@gmail.com>
2024-09-02 23:43:57 +0545

    JAVA SELF LEARNING
    JAVA SELF LEARNING

--- MyFirstProgramme/src/stringIntro.java
+++ MyFirstProgramme/src/stringIntro.java
@@ -1,5 +1,9 @@
 class stringIntro{
     public static void main(String[]args){
-        
+        String name =new String("prajjwal rimal");
+        System.out.println(name);
+        System.out.println(name.hashCode());
+        System.out.println(name.charAt(3));
+        System.out.println(name.concat(" is the best "));
     }
 }
