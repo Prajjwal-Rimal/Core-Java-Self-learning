1e892a65093957b76b7e9cd74342dc160ec3f144
Prajjwal Rimal <rimalprajjwal1@gmail.com>
2024-09-02 23:58:52 +0545

    JAVA SELF LEARNIG
    JAVA SELF LEARNIG

---
+++ MyFirstProgramme/src/stringMutableImmutable.java
@@ -0,0 +1,11 @@
+class stringMutableImmutable{
+    public static void main(String[]args){
+        String name ="prajjwal";
+        name = name+" rimal";
+        System.out.println(name);
+
+        String s1 ="Prajjwal";
+        String s2 ="Prajjwal";
+        System.out.print(s1==s2);
+    }
+}
